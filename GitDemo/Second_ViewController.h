0e16af0efd53104d7cc45262191bb9953d6bb21f
redick <redickpay@gmail.com>
2016-01-06 15:14:16 +0800

    加了第二頁

---
+++ GitDemo/Second_ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Second_ViewController.h
+//  GitDemo
+//
+//  Created by redick on 2016/1/6.
+//  Copyright © 2016年 redick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Second_ViewController : UIViewController
+
+@end
---
+++ GitDemo/Second_ViewController.m
@@ -0,0 +1,37 @@
+//
+//  Second_ViewController.m
+//  GitDemo
+//
+//  Created by redick on 2016/1/6.
+//  Copyright © 2016年 redick. All rights reserved.
+//
+
+#import "Second_ViewController.h"
+
+@interface Second_ViewController ()
+
+@end
+
+@implementation Second_ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
